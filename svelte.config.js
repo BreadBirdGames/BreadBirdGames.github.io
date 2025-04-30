5038ac445816145ca73cb5bb0024355457b5df30
cornelius <macrosedk@gmail.com>
2025-04-30 22:07:15 +0200

    Fixes i hope?

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess({ lang: "ts" }),
+  preprocess: vitePreprocess(),
   compilerOptions: {
     customElement: true,
   },
@@ -19,15 +19,12 @@ const config = {
       // these options are set automatically — see below
       pages: "build",
       assets: "build",
-      fallback: undefined,
-      precompress: false,
+      fallback: "404.html",
+      precompress: true,
       strict: true,
     }),
     paths: {
-      base:
-        process.env.NODE_ENV === "production"
-          ? "/BreadBirdGames.github.io"
-          : "",
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
     },
   },
 };
