bbc9b5c8b961420c579937a5305b8ce46cd5a957
cornelius <macrosedk@gmail.com>
2025-05-03 21:11:31 +0200

    add information to eac go

--- src/routes/games/EACGo/+page.svelte
+++ src/routes/games/EACGo/+page.svelte
@@ -5,4 +5,10 @@
 
 <navbar-component pageName="Games"></navbar-component>
 
-<h1>EAC Go</h1>
+<h1>EAC-Go</h1>
+<p>
+    is a small RPG built for Asia House to provide a brainbreak during
+    conferences, it sadly only works physically on premises
+</p>
+
+<a href="https://breadbirdgames.github.io/EAC-GO/">Link to game</a>
