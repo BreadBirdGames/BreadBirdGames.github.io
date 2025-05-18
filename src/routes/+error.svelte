1e166c8ab1b43461efad1a529f59d5d0316595e8
cornelius <macrosedk@gmail.com>
2025-05-19 00:00:36 +0200

    error handling, hope it works!!

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from "$app/state";
+
+    function test(err: App.Error | null) {
+        if (err != null) {
+            console.log(err.message);
+        }
+    }
+</script>
+
+<h1>{test(page.error)}</h1>
